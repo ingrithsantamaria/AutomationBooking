26d33629e23fbf98db19ad4086707f38edf1e75b
Ingrith Santamaria <ingrithsanta@gmail.com>
2024-03-13 14:17:26 -0500

    select dates

--- pages/dashboardPage.js
+++ pages/dashboardPage.js
@@ -1,19 +1,56 @@
 export class Dashboard {
   constructor(page) {
-    this.page = page;
+    this.page = page
+    this.calendarBtn = 'div[data-testid="searchbox-dates-container"]'
+    this.bodyCalendar = 'div[data-testid="searchbox-datepicker-calendar"]'
+    this.nextMonthBtn = 'div[data-testid="searchbox-datepicker-calendar"] >> button'
+    this.closeDismissSignInfoBtn = 'button[aria-label="Dismiss sign-in info."]'
   }
   async navigate() {
     await this.page.goto('https://www.booking.com');
+  }
+  async closeDismissSignInfo() {
+    await this.page.click(this.closeDismissSignInfoBtn)
   }
   async clickToShowListCities() {
     await this.page.click('input[name=ss]');
     await this.page.waitForSelector('div > ul', { state: 'visible' });
   }
   async selectRandomListCities() {
-    const items = await this.page.$$('div > ul > li');
-    const randomIndex = Math.floor(Math.random() * items.length);
-    const randomItem = items[randomIndex];
-    await randomItem.click();
+    const cities = await this.page.$$('div > ul > li');
+    const randomIndex = Math.floor(Math.random() * cities.length);
+    const randomCity = cities[randomIndex];
+    await randomCity.click();
   }
+  async openCalendar() {
+    await this.page.click(this.calendarBtn)
+  }
+  async selectNextMonth() {
+    // await this.openCalendar()
+    await this.page.click(this.nextMonthBtn)
+  }
+  async selectDate(day) {
+    await this.page.click(`table tbody tr td:text("${day}")`);
+  }
+  async selectStartDate() {
+    const today = new Date()
+    const nextMonth = new Date(today.getFullYear(), today.getMonth() + 1, 1)
+    const startDay = nextMonth.getDate()
+    await this.openCalendar()
+    await this.selectNextMonth()
+    await this.selectDate(startDay)
+  }
+  async selecEndDate(daysAfterStart = 7){
+    const today = new Date()
+    const endDate = new Date(today.getFullYear(), today.getMonth() +1, daysAfterStart)
+    const endDay = endDate.getDate()
+    await this.selectDate(endDay)
+  }
+//   async selectDates(startDay, endDay) {
+//     await this.openCalendar();
+//     await this.selectNextMonth();
+//     await this.selectDate(startDay);
+//     await this.selectDate(endDay);
+//   }
 }
 
--- tests/dashboardTest.spec.js
+++ tests/dashboardTest.spec.js
@@ -3,6 +3,10 @@ import { test } from '@playwright/test'
 test('select random list item', async ({ page }) => {
   const dashboardPage = new Dashboard(page);
   await dashboardPage.navigate();
+  await dashboardPage.closeDismissSignInfo()
   await dashboardPage.clickToShowListCities();
   await dashboardPage.selectRandomListCities();
+  await dashboardPage.selectStartDate()
+  await dashboardPage.selecEndDate()
+//   await dashboardPage.selectDates("10", "20")
 });
