fe417ec3685730a64a590edf2490620d5424ba72
Ingrith Santamaria <ingrithsanta@gmail.com>
2024-03-13 21:39:02 -0500

    Click on the first element and click and avalibily

--- pages/dashboardPage.js
+++ pages/dashboardPage.js
@@ -1,17 +1,21 @@
 export class Dashboard {
   constructor(page) {
-    this.page = page
-    this.calendarBtn = 'div[data-testid="searchbox-dates-container"]'
-    this.bodyCalendar = 'div[data-testid="searchbox-datepicker-calendar"]'
-    this.nextMonthBtn = 'div[data-testid="searchbox-datepicker-calendar"] >> button'
-    this.closeDismissSignInfoBtn = 'button[aria-label="Dismiss sign-in info."]'
-    this.closeSignPopup = 'button[data-command="noop"]'
+    this.page = page;
+    this.calendarBtn = 'div[data-testid="searchbox-dates-container"]';
+    this.bodyCalendar = 'div[data-testid="searchbox-datepicker-calendar"]';
+    this.nextMonthBtn =
+      'div[data-testid="searchbox-datepicker-calendar"] >> button';
+    this.closeDismissSignInfoBtn = 'button[aria-label="Dismiss sign-in info."]';
+    this.closeSignPopup = 'button[data-command="noop"]';
+    this.submitBtn = 'button[type="submit"]';
+    this.filterCheckbox = 'div input[type="checkbox"]';
+    this.availabilityBtn = 'button[data-testid="reviews-block-availability"]'
   }
   async navigate() {
-    await this.page.goto('https://www.booking.com');
+    await this.page.goto("https://www.booking.com");
   }
   async closeDismissSignInfo() {
-    await this.page.click(this.closeDismissSignInfoBtn)
+    await this.page.click(this.closeDismissSignInfoBtn);
   }
   async clickIfVisiblePopupSign() {
     if (await this.page.isVisible(this.closeSignPopup)) {
@@ -19,8 +23,8 @@ export class Dashboard {
     }
   }
   async clickToShowListCities() {
-    await this.page.click('input[name=ss]');
-    await this.page.waitForSelector('div > ul', { state: 'visible' });
+    await this.page.click("input[name=ss]");
+    await this.page.waitForSelector("div > ul", { state: "visible" });
   }
   async selectRandomListCities() {
     const cities = await this.page.$$('div > ul > li[role="option"]');
@@ -29,34 +33,46 @@ export class Dashboard {
     await randomCity.click();
   }
   async openCalendar() {
-    await this.page.click(this.calendarBtn)
+    await this.page.click(this.calendarBtn);
   }
   async selectNextMonth() {
-    // await this.openCalendar()
-    await this.page.click(this.nextMonthBtn)
+    await this.page.click(this.nextMonthBtn);
   }
   async selectDate(day) {
     await this.page.click(`table tbody tr td:has-text("${day}")`);
   }
   async selectStartDate() {
-    const today = new Date()
-    const nextMonth = new Date(today.getFullYear(), today.getMonth() + 1, 1)
-    const startDay = nextMonth.getDate()
+    const today = new Date();
+    const nextMonth = new Date(today.getFullYear(), today.getMonth() + 1, 1);
+    const startDay = nextMonth.getDate();
     // await this.openCalendar()
-    await this.selectNextMonth()
-    await this.selectDate(startDay)
+    await this.selectNextMonth();
+    await this.selectDate(startDay);
   }
-  async selecEndDate(daysAfterStart = 7){
-    const today = new Date()
-    const endDate = new Date(today.getFullYear(), today.getMonth() +1, daysAfterStart)
-    const endDay = endDate.getDate()
-    await this.selectDate(endDay)
+  async selecEndDate(daysAfterStart = 7) {
+    const today = new Date();
+    const endDate = new Date(
+      today.getFullYear(),
+      today.getMonth() + 1,
+      daysAfterStart
+    );
+    const endDay = endDate.getDate();
+    await this.selectDate(endDay);
   }
-//   async selectDates(startDay, endDay) {
-//     await this.openCalendar();
-//     await this.selectNextMonth();
-//     await this.selectDate(startDay);
-//     await this.selectDate(endDay);
-//   }
+  async clickOnSubmitButton() {
+    await this.page.click(this.submitBtn);
+    await this.page.waitForTimeout(1000);
+  }
+  async selectRandomFilter() {
+    const checkbox = await this.page.$$(this.filterCheckbox);
+    const randonIndex = Math.floor(Math.random() * checkbox.length);
+    await checkbox[randonIndex].click();
+    await this.page.waitForTimeout(4000);
+  }
+  async selectFirstElement() {
+    await page.locator('div[data-testid="property-card"]').nth(0).click()
+  }
+  async clickOnAvailabilityButton() {
+    await this.page.click(this.availabilityBtn)
+  }
 }
-
--- tests/dashboardTest.spec.js
+++ tests/dashboardTest.spec.js
@@ -1,13 +1,56 @@
 import { Dashboard } from '../pages/dashboardPage';
 import { test } from '@playwright/test'
-test('select random list item', async ({ page }) => {
+test('User select a city', async ({ page }) => {
   const dashboardPage = new Dashboard(page);
   await dashboardPage.navigate();
   await dashboardPage.closeDismissSignInfo()
   await dashboardPage.clickToShowListCities();
   await dashboardPage.selectRandomListCities();
-  await dashboardPage.clickIfVisiblePopupSign()
-  await dashboardPage.selectStartDate()
-  await dashboardPage.selecEndDate()
-//   await dashboardPage.selectDates("10", "20")
 });
+test('User select dates', async ({ page }) => {
+    const dashboardPage = new Dashboard(page);
+    await dashboardPage.navigate();
+    await dashboardPage.closeDismissSignInfo()
+    await dashboardPage.clickToShowListCities();
+    await dashboardPage.selectRandomListCities();
+    await dashboardPage.clickIfVisiblePopupSign()
+    await dashboardPage.selectStartDate()
+    await dashboardPage.selecEndDate()
+  });
+  test('User clicks on Submit', async ({ page }) => {
+    const dashboardPage = new Dashboard(page);
+    await dashboardPage.navigate();
+    await dashboardPage.closeDismissSignInfo()
+    await dashboardPage.clickToShowListCities();
+    await dashboardPage.selectRandomListCities();
+    await dashboardPage.clickIfVisiblePopupSign()
+    await dashboardPage.selectStartDate()
+    await dashboardPage.selecEndDate()
+    await dashboardPage.clickOnSubmitButton()
+    await dashboardPage.selectRandomFilter()
+  });
+  test('User clicks on filter', async ({ page }) => {
+    const dashboardPage = new Dashboard(page);
+    await dashboardPage.navigate();
+    await dashboardPage.closeDismissSignInfo()
+    await dashboardPage.clickToShowListCities();
+    await dashboardPage.selectRandomListCities();
+    await dashboardPage.clickIfVisiblePopupSign()
+    await dashboardPage.selectStartDate()
+    await dashboardPage.selecEndDate()
+    await dashboardPage.clickOnSubmitButton()
+    await dashboardPage.selectRandomFilter()
+  });
+  test('User clicks on the first result', async ({ page }) => {
+    const dashboardPage = new Dashboard(page);
+    await dashboardPage.navigate();
+    await dashboardPage.closeDismissSignInfo()
+    await dashboardPage.clickToShowListCities();
+    await dashboardPage.selectRandomListCities();
+    await dashboardPage.clickIfVisiblePopupSign()
+    await dashboardPage.selectStartDate()
+    await dashboardPage.selecEndDate()
+    await dashboardPage.clickOnSubmitButton()
+    await dashboardPage.selectRandomFilter()
+    await dashboardPage.selectFirstElement()
+  });
